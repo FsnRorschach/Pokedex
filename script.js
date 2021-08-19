11e24743553858071fcb768c314b58a651d92f33
FABIO SANTOS NOVAIS <9411909-FsnRorschach@users.noreply.gitlab.com>
2021-08-19 15:37:15 -0300

    Pokedex e melhoria no random

--- script.js
+++ script.js
@@ -1,75 +1,89 @@
 $(document).ready(() => {
-	let randomID = 0;
+	
 	let imgConteiner = document.querySelector('#divShowImg');
-	let container = document.querySelector('#divShowPokemon');
-	let btnConteiner = document.querySelector('#divChoiceBtn');
-
-	let initGame = document.getElementById('initGame');
+	let container = document.querySelector('.containerPokedex');
 
 	$('#initGame').click(event => {
 		event.preventDefault();
 
-		$('#divInitGame').hide();
-		$('#divLogo').hide();
+		$('.homeScreem').hide();
 
-		$('#divShowImg').show();
-		$('#divShowPokemon').show();
-		$('#divChoiceBtn').show();
+		$('.gameScreem').show();
 		buscaPokemon();
-
-		setTimeout(function () {
+		
+		setTimeout(function(){
 			pokeID =  getRandomInt(min, max);
-			console.log(pokeID);
 			let nomePoke = pokemon.results[pokeID-1].name;
-			console.log(nomePoke);
-			if (nomePoke == null || nomePoke == undefined){
-				pokemonUndefined = pokeID;
-				pokeID = getRandomInt(min, max);
-			}
-			
 
-			//let iDPoke = pokeID;
-			//let imgPoke = pokemon.sprites.front_default;
-			let elist = document.querySelectorAll('button');
-			let pokeTag = document.createElement('img');
-			
-			for(let i = 4; i>0;i--){
-				console.log(elist);
-				if(i == 4){
-					let ind = getRandomInt(1, i);
-					console.log(ind);
+			// let elist = document.querySelectorAll('button');
+
+			// let pokeTag = document.createElement('img');
+
+			// let tagUl = document.createElement('ul');
+			// let tagLi = document.createElement('li');
+			// let tagFigurePoke = document.createElement('figure');
+			// let tagImgPoke = document.createElement('img');
+			// let tagDivInfo = document.createElement('div');
+			// let tagPInfo = document.createElement('p');
+			// let tagSpanInfo = document.createElement('span');
+			// let tagh5Info = document.createElement('h5');
+			// let tagDivAbilitys1 = document.createElement('div');			
+			// let tagSpanAbilitys1 = document.createElement('span');
+			// let tagDivAbilitys2 = document.createElement('div');			
+			// let tagSpanAbilitys2 = document.createElement('span');
+
+		
+						
+			for(let i = 2; i>0;i--){
+				if(i == 2){
+					let ind = getRandomInt(1, 4);
 					$('#btn'+ ind).val(pokeID).html(nomePoke);
-					
-					//pokeTag.setAttribute('src',imgPoke);
-					//pokeTag.setAttribute('id','imgPokeTag');
-					//pokeTag.setAttribute('alt',nomePoke);
-					//pokeTag.append(imgPoke);
-
-					//imgConteiner.appendChild(pokeTag);
+					let pokeFields = "https://pokeapi.co/api/v2/pokemon/";
+					setInterval( function(){
+						requestPokeInfo(String(pokeFields + pokeID)
+						)},200);
 				}else{
 					$('#divChoiceBtn button').each(function(index, element) {
 						if(element.value == ""){
 							let provId = getRandomInt(min, max);
 							if(provId != pokeID){
 								nomePoke = pokemon.results[provId-1].name;
-								console.log(provId +" id prov. e nome pokemon "+ nomePoke);
-								$('#btn'+ index).val(provId).html(nomePoke);
+								$('#btn'+ (index+1)).val(provId).html(nomePoke);
 							}else{
 								provId = provId+1;
-								nomePoke = pokemon.results[provId-1].name;
-								console.log(provId +" id prov. e nome pokemon "+ nomePoke);
-								$('#btn'+ index).val(provId).html(nomePoke);
+								nomePoke = pokemon.results[provId-1].name; 
+								$('#btn'+ (index+1)).val(provId).html(nomePoke);
 							}
 						}
 					});				
 				}
 			}
-		}, 2000);
+			
+			let imgPoke = pokeAbilitys.sprites.front_default;	
+			pokeTag.setAttribute('src',imgPoke);
+			pokeTag.append(imgPoke);
+			console.log(pokeTag);
+
+			imgConteiner.appendChild(pokeTag);
+			container.appendChild(pokeTag);
+
+			
+			console.log(' teste saber quando carrega: dentro do settimeout '+ pokeAbilitys);
+			tagUl.setAttribute('class','pokedexResults');
+			tagLi.setAttribute('class','figures');
+			//tagImgPoke.setAttribute('src',imgPoke);
+			tagDivInfo.setAttribute('class','div-poke-info');
+			tagPInfo.setAttribute('class','id-poke-info');
+			tagSpanInfo.setAttribute('class','id-number');
+			tagDivAbilitys1.setAttribute('class','abilitys1');					
+			tagDivAbilitys2.setAttribute('class','abilitys2');
+		},4000);
 	});
 });
 
 let pokeID;
 let pokemon;
+let pokeAbilitys;
 let pokemonUndefined = [];
 let Pokedex = [];
 
@@ -78,30 +92,81 @@ const min = 1;
 const max = 150;
 
 //API
-const baseUrl =  'https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0';
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0';
 
 
 // Busca Pokemon
-function buscaPokemon(){
-	requestPokeInfo(baseUrl);	
+async function buscaPokemon(){
+	try{
+		await requestPokeInfo(baseUrl);	
+	}
+	catch(err){
+		console.log('Falha na consulta', err);
+	}
+	
 }
 
 // Busca numero aleatorio
 function getRandomInt( min , max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;  
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;  
 }
 
-function requestPokeInfo(url) {
-	fetch(url)
-	.then(response => response.json())
-	.then(data => {
-	pokemon = data;
-
-	console.log(data);
-    })
-	.catch(function(error) {
-		console.log('Erro na busca da API Pokemon: ' + error.message);
-	  });
+async function requestPokeInfo(url) {
+	console.log('entrou na requestpoke');
+	try {
+		await fetch(url)
+		.then(response => {
+			// valida se a requisição falhou
+			console.log(response);
+			if (!response.ok) {
+				return new Error('falhou a requisição') // cairá no catch da promise
+			}	
+			// verificando pelo status
+			if (response.status === 404) {
+				return new Error('não encontrou qualquer resultado')
+			}
+			return response.json();})
+		.then(data => {
+			if(url != baseUrl){
+				pokeAbilitys = data;
+				console.log('dentro da request , recebe data '+ pokeAbilitys);
+			}
+			else{
+				pokemon = data;
+				
+				console.log(pokemon);		
+			}	
+    	});
+	} catch (error) {
+		console.log('Erro na busca fetch da API Pokemon: ' + error.message);
+	}
 }
+
+// async function callService(url) {
+//     var response = null; 
+
+//     try {
+//         response = await fetch(url).then(res => res.json());
+//     } catch(err) {
+//         response = err;
+//     }
+
+//     return response;
+// }
+
+// async function getPokemonData() {
+//     try {
+//         const response = await callService('https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0');
+//         const { results } = response;
+
+//         for (var i = 0; i < 10; i++) {
+//             var pokemonData = await callService(results[i].url);
+//             console.log('Exibindo data do pokemon ' + results[i].name);
+//             console.log(pokemonData);
+//         }
+//     } catch(err) {
+//         console.log('Falha na consulta', err);
+//     }
+// }
